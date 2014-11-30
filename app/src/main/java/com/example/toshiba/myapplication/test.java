0d4b91a842d436edc9478ee5a6272cf44a1840c9
dreadful44 <iletisim@korhanozbek.com>
2014-11-30 17:25:33 +0200

    Korhan ÖZBEK

--- app/src/main/java/com/example/toshiba/myapplication/test.java
+++ app/src/main/java/com/example/toshiba/myapplication/test.java
@@ -4,4 +4,7 @@ package com.example.toshiba.myapplication;
  * Created by Korhan on 30.11.2014.
  */
 public class test {
+    public void deneme (){
+        System.out.println("asdfjksda");
+    }
 }
