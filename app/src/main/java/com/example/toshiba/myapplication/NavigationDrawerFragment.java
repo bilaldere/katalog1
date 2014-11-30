6f80c8b1411d9c9c69396800c36b97e92c0bcd6d
dreadful44 <iletisim@korhanozbek.com>
2014-11-30 17:26:17 +0200

    Korhan ÖZBEK

--- app/src/main/java/com/example/toshiba/myapplication/NavigationDrawerFragment.java
+++ app/src/main/java/com/example/toshiba/myapplication/NavigationDrawerFragment.java
@@ -105,6 +105,7 @@ public class NavigationDrawerFragment extends Fragment {
                         getString(R.string.title_section1),
                         getString(R.string.title_section2),
                         getString(R.string.title_section3),
+                        "yeşil kapı"
 
                 }));
         mDrawerListView.setItemChecked(mCurrentSelectedPosition, true);
